b959c601869447394cffb4e053864dbdfcc0c2f5
MarkSlavenko <markslavenkooo@gmail.com>
2020-08-05 14:16:45 +0300

    Connect Redux with Cart page

--- client/src/index.js
+++ client/src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import 'normalize.css';
+import { Provider } from 'react-redux';
 import App from './App';
 import './index.scss';
+import { store } from './store/configureStore';
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </BrowserRouter>,
   document.getElementById('root'),
 );
--- client/src/pages/Cart/index.js
+++ client/src/pages/Cart/index.js
@@ -1,48 +1,29 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import ProductsList from '../../components/ProductsList';
-
-const productsExample = [
-  {
-    id: "sfwesr32r3efer",
-    img: "https://images.ua.prom.st/1764714745_w640_h640_belye-krossovki-zhenskie.jpg",
-    title: "Naming test olyalya 1",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore" + 
-    "et dolore magna aliqua.",
-    count: 1,
-    price: 25.99,
-  },
-  {
-    id: "1231231232r3efer",
-    img: "https://i8.rozetka.ua/goods/16407196/174533913_images_16407196037.jpg",
-    title: "Naming test olyalya 2",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore" + 
-    "et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea " +
-    "commodo consequat.",
-    count: 3,
-    price: 40.00,
-  },
-  {
-    id: "sfwesr3453452677gfer",
-    img: "https://images.ua.prom.st/1531243327_zhenskie-krossovki-queencity.jpg",
-    title: "Naming test olyalya 3",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore" + 
-    "et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut.",
-    count: 2,
-    price: 20.00,
-  },
-];
 
 class Cart extends Component { 
 
   render () {
+    const { cartItems, totalPrice} = this.props;
+
     return (
       <div className="cart-page container">
         <ProductsList 
-        products={productsExample} 
+        products={cartItems}
+        totalPrice={totalPrice}
         />
       </div>
     );
   }
 }
 
-export default Cart;
+const mapStateToProps = (store) => ({
+  cartItems: store.cart.cartItems,
+  totalPrice: store.cart.totalPrice,
+});
+
+export default connect(
+  mapStateToProps,
+  // mapDispatchToProps,
+)(Cart);
